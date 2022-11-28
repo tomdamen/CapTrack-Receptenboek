943797251174b2d52b227c7e9e53808864beaf76
Tom Damen <twjdamen@gmail.com>
2022-11-28 16:27:45 +0100

    made page interaction

--- php/functions.php
+++ php/functions.php
@@ -5,7 +5,9 @@
 function makeRecipeGrid(array $recipeArray) : string {
     $returnString = "";
     foreach ($recipeArray as $recipe) {
-        $returnString = $returnString . '<a href="./recipe.php" class="home-recipe"><img src="./images/' . removeSpaces($recipe["title"]) . '.jpg" alt=""><p class="text-center">' .  $recipe["title"] . '</p></a>';
+        $returnString = $returnString . '<a href="./recipe.php?id=' . $recipe["id"] 
+        . '" class="home-recipe"><img src="./images/' . removeSpaces($recipe["title"]) 
+        . '.jpg" alt=""><p class="text-center">' .  $recipe["title"] . '</p></a>';
     }    
     return $returnString;
 }
@@ -19,6 +21,9 @@ function createListFromArray($array) {
     foreach ($array as $line) {
         $outputString = $outputString . "<li>" . $line . "</li>";
     }
-
     return $outputString;
+}
+
+function splitOnNewLine(string $inputString) : array {
+    return preg_split("/\r\n|\n|\r/", $inputString);
 }
